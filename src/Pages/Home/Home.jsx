65af0430a24038e5efef121360b85abbe7573fc0
Abin Rajesh <abinrajeshh@gmail.com>
2025-09-21 15:18:42 +0530

    Completed the design of Profile Page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,18 +3,16 @@ import Header from '../../Components/Header/Header'
 import Category from '../../Components/Category/Category'
 import MobileAppBanner from '../../Components/Banner/MobileAppBanner'
 import Footer from '../../Components/Footer/Footer'
-import Ads from '../../Components/Ads/Ads'
-import { Outlet } from 'react-router-dom';
+// import Ads from '../../Components/Ads/Ads'
+// import { Outlet } from 'react-router-dom';
+import Profile from '../../Components/Profile/Profile'
 
 const Home = () => {
     return (
         <div>
             <Header />
             <Category />
-            <MobileAppBanner />
-            <Outlet />
-            <Ads />
-            <MobileAppBanner />
+            <Profile />
             <Footer />
         </div>
     )
